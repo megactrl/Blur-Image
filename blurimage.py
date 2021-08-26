1f2cc4a1205d53adeebc53ef8803832e1c7d6bb2
Hitesh Kumar <42475956+megactrl@users.noreply.github.com>
2021-08-26 23:05:02 +0530

    Update blurimage.py

--- blurimage.py
+++ blurimage.py
@@ -1,5 +1,6 @@
 import cv2
-img = cv2.imread('me.jpg')
+img = cv2.imread('me.jpg') //read the image
+//Blur Image Through Averaging 
 blurImg = cv2.blur(img,(10,10))
 cv2.imshow('Blurred image', blurImg)
 cv2.waitKey(0)
